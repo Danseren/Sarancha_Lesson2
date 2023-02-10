e4f7a3d44f9c14ec6a66ba5b238b1e3b30494105
Denis <etlendo@mail.ru>
2023-02-10 13:35:04 +0500

    Work | Макет приложения, добавил второе Activity, отметил его в Манифесте, подключил binding, организовал переход между Activity

--- app/src/main/java/ru/aston/sarancha_lesson2/MainActivity.kt
+++ app/src/main/java/ru/aston/sarancha_lesson2/MainActivity.kt
@@ -1,12 +1,22 @@
 package ru.aston.sarancha_lesson2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.aston.sarancha_lesson2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnGoToSecondActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
